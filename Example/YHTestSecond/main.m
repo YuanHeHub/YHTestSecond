4a779d58ff4cc8dfcdac983326a278ee023488d1
YuanHeHub <15237107499@163.com>
2021-06-23 19:45:05 +0800

    Initial commit

---
+++ Example/YHTestSecond/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  YHTestSecond
+//
+//  Created by 15237107499@163.com on 06/23/2021.
+//  Copyright (c) 2021 15237107499@163.com. All rights reserved.
+//
+
+@import UIKit;
+#import "YHAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([YHAppDelegate class]));
+    }
+}
